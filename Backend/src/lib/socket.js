3baf8b01a6079bcd8dffd2265e72cc83eb563b93
Muhammad Ali <Muhammadaliiqbal02@gmail.com>
2026-06-06 15:55:03 +0500

    Backend

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://localhost", "http://127.0.0.1:5173","https://chit-chat-app-five.vercel.app"],
     credentials: true,
     methods: ["GET", "POST"]
   },
